eb5a8c45c85245e6a1a60e8a29a2d68a58489571
Tkachenko Natalia <nataprofits@gmail.com>
2023-06-01 21:50:31 +0100

    btn style

--- src/components/FeedbackOptions/FeedBackOptions.styled.js
+++ src/components/FeedbackOptions/FeedBackOptions.styled.js
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 
-const color = 'white';
+const color = 'black';
 
 const setBgColor = props => {
     switch (props.name) {
@@ -24,11 +24,23 @@ justify-content: space-around;
 export const Item = styled.li``;
 
 export const Button = styled.button`
-padding: 5px;
-      background-color: ${setBgColor};
-      font-size: 24px;
-      border-radius: 4px;
-      &:hover {
-        color: ${color};
-      }
+
+background: #ECECEC;
+	border-radius: 15px;
+	padding: 10px 20px;
+	display: block;
+	font-family: arial;
+	font-weight: bold;
+	color:#7f7f7f;
+	text-decoration: none;
+	border:1px solid #a7a7a7;
+	width: 145px;
+	margin:0px auto;
+	margin-top:100px;
+	box-shadow: 0px 2px 1px white inset, 0px -2px 8px white, 0px 2px 5px rgba(0, 0, 0, 0.1), 0px 8px 10px rgba(0, 0, 0, 0.1);
+	-webkit-transition:box-shadow 0.5s;
+&:hover {
+    background-color: ${setBgColor};
+    color: ${color};
+        }
 `;
